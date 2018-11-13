3e40eb2103fe5cc735f5ddbfe4a39e02be61f9bd
aderito xavier <aderito_xavier@sapo.pt>
2018-11-13 21:41:05 +0000

    store inserted

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from './components/App';
+import rootReducer from './reducers';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
+const store = createStore(rootReducer);
+
+console.log(store.getState());
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+    , document.getElementById('root')
+);
 
