8ebd489fb90a327ed3691cf3f1aa94848620d724
aderito xavier <aderito_xavier@sapo.pt>
2018-12-11 11:40:14 +0000

    style changes in supersquad index page

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
 import CharacterList from './CharacterList';
-
+import '../styles/index.css';
 class App extends Component {
     render() {
         return (
-            <div>
+            <div className="App">
                 <h2>SuperSquad</h2>
                 <CharacterList/>
             </div>
